4d06a210ed5ba8fb57902cb9f019e5f8667cb419
Alphonse Mariya <alphonse.mariya@hotmail.com>
2019-03-29 12:08:31 +0100

    Use BFS to calculate distances to items

--- amariy_A1.py
+++ amariy_A1.py
@@ -17,6 +17,7 @@ so they are anonymous (and your 'name' is expected to be funny, no pressure).
 """
 
 # from config import *
+from collections import deque
 from contextlib import contextmanager
 from operator import add, attrgetter
 from random import sample
@@ -55,17 +56,17 @@ MOVE_LEFT = -2
 MOVE_RIGHT = 2
 
 #[Awards]
-AWARD_RHUBARB = 5
-AWARD_GRASS = 1
+AWARD_RHUBARB = 5.0
+AWARD_GRASS = 1.0
 
 # Constants
 ###############################################################################
 
 #[Awards]
-AWARD_SHEEP = 100
+AWARD_SHEEP = 100.0
 
 #[Penalty]
-PENALTY_MOVE_NONE = -1
+PENALTY_MOVE_NONE = -1.0
 PENALTY_NEAR_WOLF = -float("inf")
 
 #[Movements]
@@ -131,56 +132,68 @@ def sheep_score(playfield, player_nr):
     Returns score for current position of the sheep of player_nr.
     """
 
-    score = 0
     sheep = playfield.get_sheep(player_nr)
     enemy_wolf = playfield.get_wolf(player_nr % 2 + 1)
-    if sheep in playfield.rhubarb:
-        playfield.rhubarb.remove(sheep)
-        score = AWARD_RHUBARB
-    if sheep in playfield.grass:
-        playfield.grass.remove(sheep)
-        score = AWARD_GRASS
-
-    rhubarb_path = [astar(playfield.figure + playfield.fence, sheep, i)
-                    for i in playfield.rhubarb]
-    grass_path = [astar(playfield.figure + playfield.fence, sheep, i)
-                  for i in playfield.grass]
+    enemy_sheep = playfield.get_sheep(player_nr % 2 + 1)
 
     if is_near(sheep, enemy_wolf):
-        score = PENALTY_NEAR_WOLF
-    else:
-        score += sum(map(lambda x: AWARD_RHUBARB / (len(x) + 1), rhubarb_path)) \
-               + sum(map(lambda x: AWARD_GRASS / (len(x) + 1), grass_path))
+        return PENALTY_NEAR_WOLF
 
-    return score
+    if not playfield.items:
+        enemy_sheep_path = astar([enemy_sheep, sheep] + playfield.fences, sheep, enemy_sheep)
+        return 1 / len(enemy_sheep_path) + wolf_score(playfield, player_nr)
+
+    # Score if sheep is on item
+    score = playfield.items.pop(sheep) if sheep in playfield.items else 0
+
+    # Paths
+    item_path = bfs(playfield.figures + playfield.fences, sheep, playfield.items)
+    enemy_item_path = bfs(playfield.figures + playfield.fences, enemy_sheep, playfield.items)
+
+    # Scores
+    item_scores = dict(map(lambda a: (a[0], playfield.items[a[0]] / len(a[1])),
+                           item_path.items()))
+    enemy_item_scores = dict(map(lambda a: (a[0], playfield.items[a[0]] / (len(a[1]) + 1)),
+                                 enemy_item_path.items()))
+
+    return score + sum([(item_scores[k] - enemy_item_scores[k]) for k in playfield.items])
 
 def wolf_score(playfield, player_nr):
     """
     Returns score for current position of the wolf of player_nr.
     """
 
-    score = 0
     wolf = playfield.get_wolf(player_nr)
     sheep = playfield.get_sheep(player_nr)
+    enemy_wolf = playfield.get_wolf(player_nr % 2 + 1)
     enemy_sheep = playfield.get_sheep(player_nr % 2 + 1)
-    if wolf in playfield.rhubarb:
-        playfield.rhubarb.remove(wolf)
-        score = AWARD_RHUBARB
-    if wolf in playfield.grass:
-        playfield.grass.remove(wolf)
-        score = AWARD_GRASS
 
-    sheep_path = astar(playfield.figure + playfield.fence, wolf, sheep)
-    enemy_sheep_path = astar(playfield.figure + playfield.fence, wolf, enemy_sheep)
+    if wolf == enemy_sheep:
+        return AWARD_SHEEP
 
-    score += AWARD_SHEEP / (len(enemy_sheep_path) + 1)
+    # Paths
+    sheep_path = astar([enemy_wolf, enemy_sheep] + playfield.fences, wolf, sheep)
+    enemy_sheep_path = astar([enemy_sheep, sheep] + playfield.fences, wolf, enemy_sheep)
+
+    # Score for enemy_sheep distance
+    score = AWARD_SHEEP / len(enemy_sheep_path)
+
+    # Score if wolf is on item
+    if wolf in playfield.items:
+        score += playfield.items.pop(wolf)
+
     if len(sheep_path) > len(enemy_sheep_path):
-        rhubarb_path = [astar(playfield.figure + playfield.fence, wolf, i)
-                        for i in playfield.rhubarb]
-        grass_path = [astar(playfield.figure + playfield.fence, wolf, i)
-                      for i in playfield.grass]
-        score += sum(map(lambda x: AWARD_RHUBARB / (len(x) + 1), rhubarb_path)) \
-               + sum(map(lambda x: AWARD_GRASS / (len(x) + 1), grass_path))
+        # Paths
+        item_path = bfs(playfield.figures + playfield.fences, sheep, playfield.items)
+        enemy_item_path = bfs(playfield.figures + playfield.fences, enemy_sheep, playfield.items)
+
+        # Scores
+        item_scores = dict(map(lambda a: (a[0], playfield.items[a[0]] / len(a[1]) + 1),
+                               item_path.items()))
+        enemy_item_scores = dict(map(lambda a: (a[0], playfield.items[a[0]] / (len(a[1]))),
+                                     enemy_item_path.items()))
+
+        score += sum([enemy_item_scores[k] - item_scores[k] for k in playfield.items])
 
     return score
 
@@ -205,8 +218,7 @@ class Node():
 def astar(obstacles, start, end):
     """
     Returns a list of tuples (x, y) indicating coordinates, as a path from the
-    given start (excluding) to the given end (including) considering the given
-    obstacles.
+    given start to the given end considering the given obstacles.
     """
 
     if start in obstacles:
@@ -216,41 +228,69 @@ def astar(obstacles, start, end):
 
     start_node = Node(None, start)
     end_node = Node(None, end)
-    todo_nodes = [start_node]
-    done_coords = []
+    queue = [start_node]
+    seen = []
 
-    while todo_nodes:
-        node = min(todo_nodes, key=attrgetter("f"))
-        todo_nodes.remove(node)
-        done_coords.append(node.coord)
+    while queue:
+        node = min(queue, key=attrgetter("f"))
+        queue.remove(node)
+        seen.append(node.coord)
 
         # Found end_node, return path excluding start_node
         if node == end_node:
-            return [n.coord for n in gen_parents(node)][:0:-1]
+            return [n.coord for n in gen_parents(node)][::-1]
 
         # Try each direction
-        for move in MOVE_COORDS:
-            coord = tuple(map(add, node.coord, move))
-
-            # Check done_coords, range and obstacles
-            if coord in done_coords:
-                continue
+        for coord in [tuple(map(add, node.coord, i)) for i in MOVE_COORDS]:
+            # Check seen, range and obstacles
             if not (0 <= coord[0] < FIELD_WIDTH and 0 <= coord[1] < FIELD_HEIGHT):
                 continue
-            if coord in obstacles:
+            if coord in seen or coord in obstacles:
                 continue
 
             new_node = Node(node, coord)
 
-            # Node is already in todo_nodes
-            for open_node in todo_nodes:
+            # Node is already in queue
+            for open_node in queue:
                 if new_node == open_node and new_node.g > open_node.g:
                     break
-            # Node is not yet in todo_nodes
+            # Node is not yet in queue
             else:
                 new_node.h = sum(map(distance, new_node.coord, end_node.coord))
                 new_node.f = new_node.g + new_node.h
-                todo_nodes.append(new_node)
+                queue.append(new_node)
+
+# Breadth First Search (BFS) algorithm
+###############################################################################
+
+def bfs(obstacles, start, search_coords):
+    """
+    Returns a dictionary with tuples (x, y) as keys indicating coordinates, and
+    a list of tuples (x, y) as values indicating the path from the given start
+    to the key's cordinate considering the given obstacles.
+    """
+
+    queue = deque([[start]])
+    seen = set([start])
+    paths = {}
+
+    while queue:
+        path = queue.popleft()
+        current = path[-1]
+
+        if current in search_coords:
+            paths[current] = path
+
+        for coord in [tuple(map(add, current, i)) for i in MOVE_COORDS]:
+            if not (0 <= coord[0] < FIELD_WIDTH and 0 <= coord[1] < FIELD_HEIGHT):
+                continue
+            if coord in seen or coord in obstacles:
+                continue
+
+            queue.append(path + [coord])
+            seen.add(coord)
+
+    return paths
 
 # Playfield class
 ###############################################################################
@@ -261,46 +301,49 @@ class Playfield:
     """
 
     def __init__(self, field):
-        self.figure = 4 * [None]
-        self.grass = []
-        self.rhubarb = []
-        self.fence = []
+        self.figures = 4 * [None]
+        self.items = {}
+        self.fences = []
 
         # Parse field strings
         for y, line in enumerate(field):
             for x, char in enumerate(line):
                 if char == CELL_SHEEP_1:
-                    self.figure[0] = (x, y)
+                    self.figures[0] = (x, y)
                 elif char == CELL_SHEEP_2:
-                    self.figure[1] = (x, y)
+                    self.figures[1] = (x, y)
                 elif char == CELL_WOLF_1:
-                    self.figure[2] = (x, y)
+                    self.figures[2] = (x, y)
                 elif char == CELL_WOLF_2:
-                    self.figure[3] = (x, y)
+                    self.figures[3] = (x, y)
                 elif char == CELL_GRASS:
-                    self.grass.append((x, y))
+                    self.items[(x, y)] = AWARD_GRASS
                 elif char == CELL_RHUBARB:
-                    self.rhubarb.append((x, y))
+                    self.items[(x, y)] = AWARD_RHUBARB
                 elif char == CELL_FENCE:
-                    self.fence.append((x, y))
+                    self.fences.append((x, y))
+                elif char == CELL_SHEEP_1_d:
+                    self.figures[0] = (x, y)
+                elif char == CELL_SHEEP_2_d:
+                    self.figures[1] = (x, y)
 
     def get_sheep(self, player_nr):
-        return self.figure[player_nr - 1]
+        return self.figures[player_nr - 1]
 
     def get_wolf(self, player_nr):
-        return self.figure[player_nr + 1]
+        return self.figures[player_nr + 1]
 
     def is_coord_available(self, coord):
-        return not (coord in self.fence
-                    and 0 <= coord[0] < FIELD_WIDTH
-                    and 0 <= coord[1] < FIELD_HEIGHT)
+        return not coord in self.fences \
+                and 0 <= coord[0] < FIELD_WIDTH \
+                and 0 <= coord[1] < FIELD_HEIGHT
 
     def simulate_sheep(self, player_nr, coord):
         """
         Simulate sheep movement, returns copy of playfield.
         """
         playfield = copy.deepcopy(self)
-        playfield.figure[player_nr - 1] = coord
+        playfield.figures[player_nr - 1] = coord
         return playfield
 
     def simulate_wolf(self, player_nr, coord):
@@ -308,7 +351,7 @@ class Playfield:
         Simulate wolf movement, returns copy of playfield.
         """
         playfield = copy.deepcopy(self)
-        playfield.figure[player_nr + 1] = coord
+        playfield.figures[player_nr + 1] = coord
         return playfield
 
 # Agent
@@ -332,23 +375,25 @@ class Alfunx():
         playfield = Playfield(field)
         coord = (0, 0)
         score = sheep_score(playfield, player_nr)
-        if playfield.grass or playfield.rhubarb:
+        if playfield.items:
             score += PENALTY_MOVE_NONE
-        print("score:", score)
+        print("\nplayer:", player_nr)
+        print("score:", coord, score)
 
         for move in sample(MOVE_COORDS, len(MOVE_COORDS)):
             new_coord = tuple(map(add, playfield.get_sheep(player_nr), move))
             if not playfield.is_coord_available(new_coord):
                 continue
-            if new_coord in playfield.figure:
+            if new_coord in playfield.figures:
                 continue
             p = playfield.simulate_sheep(player_nr, new_coord)
             s = sheep_score(p, player_nr)
-            print("score:", s, move, new_coord)
+            print("score:", move, s)
             if s > score:
                 score = s
                 coord = move
 
+        print("go:   ", coord)
         return COORD_TO_MOVE_CONST[coord]
 
     def move_wolf(self, player_nr, field):
@@ -359,7 +404,8 @@ class Alfunx():
         playfield = Playfield(field)
         coord = (0, 0)
         score = wolf_score(playfield, player_nr) + PENALTY_MOVE_NONE
-        print("score:", score)
+        print("\nplayer:", player_nr)
+        print("score:", coord, score)
 
         for move in sample(MOVE_COORDS, len(MOVE_COORDS)):
             new_coord = tuple(map(add, playfield.get_wolf(player_nr), move))
@@ -369,9 +415,10 @@ class Alfunx():
                 continue
             p = playfield.simulate_wolf(player_nr, new_coord)
             s = wolf_score(p, player_nr)
-            print("score:", s, move, new_coord)
+            print("score:", move, s)
             if s > score:
                 score = s
                 coord = move
 
+        print("go:   ", coord)
         return COORD_TO_MOVE_CONST[coord]
